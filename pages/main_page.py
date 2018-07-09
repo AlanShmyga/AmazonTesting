d07cb7238ecf34551af38475cb813c408f9fceb2
AlanShmyga <alexey.shmyga@gmail.com>
2018-07-09 22:04:12 +0300

    [CR354] Fixing the application invocation logic

--- pages/main_page.py
+++ pages/main_page.py
@@ -12,8 +12,11 @@ class MainPage:
     def open_account_list(self):
         actions = ActionChains(self.driver)
 
-        self.driver.get("https://www.amazon.com")
+        self.open_main_page()
 
         actions.move_to_element(self.driver.find_element_by_id(self.account_list)).perform()
         your_lists = AccountMenuPage(self.driver).get_your_list()
         return [element.get_attribute("innerText") for element in your_lists]
+
+    def open_main_page(self):
+        self.driver.get("https://www.amazon.com")
